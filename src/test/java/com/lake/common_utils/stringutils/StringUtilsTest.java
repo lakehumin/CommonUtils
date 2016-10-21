7ee256c46168f626ebd24ed67645f7ed471cf476
lakehumin <lakehumin@163.com>
2016-10-21 12:41:41 +0800

    start

---
+++ src/test/java/com/lake/common_utils/stringutils/StringUtilsTest.java
@@ -0,0 +1,17 @@
+package com.lake.common_utils.stringutils;
+
+import org.junit.Test;
+
+/**
+ * @author LakeHm
+ *
+ * 2016年10月21日下午12:18:55
+ */
+public class StringUtilsTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
