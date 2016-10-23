a94fd872f7b131dd507bd7eea6e79bf69f053c66
lakehumin <lakehumin@163.com>
2016-10-23 19:30:10 +0800

    update

---
+++ src/main/java/com/lake/common_utils/db_utils/DBConnectionPool.java
@@ -0,0 +1,10 @@
+package com.lake.common_utils.db_utils;
+
+/**
+ * @author LakeHm
+ *
+ * 2016年10月23日下午7:16:35
+ */
+public class DBConnectionPool {
+
+}
---
+++ src/main/java/com/lake/common_utils/db_utils/DBUtils.java
@@ -0,0 +1,10 @@
+package com.lake.common_utils.db_utils;
+
+/**
+ * @author LakeHm
+ *
+ * 2016年10月23日下午7:18:35
+ */
+public class DBUtils {
+	private String url;
+}
